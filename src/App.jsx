34aed3f323b89c560e5c9c78b8918fc9a8ca2221
fort4 <adcretary@gmail.com>
2025-06-25 04:48:15 +0900

    foundation Structured and Add React-Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import UnityPlayer from './components/UnityPlayer'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Simulation from './pages/Simulation';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>Unity WebGL 시뮬레이터</h1>
-      <UnityPlayer />
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+        <Link to="/" className="navbar-brand">Unity Ship</Link>
+        <div className="navbar-nav">
+          <Link to="/" className="nav-link">소개</Link>
+          <Link to="/simulation" className="nav-link">시뮬레이션 체험</Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/simulation" element={<Simulation />} />
+      </Routes>
+    </>
   );
 }
-
-export default App
